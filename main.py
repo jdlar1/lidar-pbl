ff651f6f5fd5d23283b8dfe7eaa3139494128187
Juan Diego <jdidelarc@gmail.com>
2022-03-22 20:10:12 -0500

    :art: Apply black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from lidar_pbl.utils import read_npz, plot_profile, rcs, quicklook, txt_to_npz
 
-data, dates = read_npz('./data/2021/08/12/RS/data.npz')
+data, dates = read_npz("./data/2021/08/12/RS/data.npz")
 data_rcs = rcs(data)
 
 quicklook(data[:-40, :], bin_res=3.75, max_height=1_500, bin_zero=0)
