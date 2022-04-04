9a3bcbfed4bfa3db638fd94218d1e94de4647265
Juan Diego <jdidelarc@gmail.com>
2022-04-03 19:54:21 -0500

    :bookmark: Overwrite version by toml file

--- lidar_pbl/__init__.py
+++ lidar_pbl/__init__.py
@@ -1,9 +1,18 @@
-__version__ = "0.1.0"
+import toml
+from pathlib import Path
+
 
 import matplotlib
 
 from .core.lidar_dataset import LidarDataset
 from .utils import in_wsl
+
+def get_version():
+   path = Path(__file__).resolve().parents[1] / 'pyproject.toml'
+   pyproject = toml.loads(open(str(path)).read())
+   return pyproject['tool']['poetry']['version']
+
+__version__ = get_version()
 
 if in_wsl():
     matplotlib.use("GTK3Agg")
